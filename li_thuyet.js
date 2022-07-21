1d5475b5463110c4993ffb7e332a5ffe78d1ecd2
dangdinhloc1997 <104234879+dangdinhloc1997@users.noreply.github.com>
2022-07-21 10:37:48 +0700

    demo

---
+++ li_thuyet.js
@@ -0,0 +1,19 @@
+
+
+function User(firstName, lastName, avartar) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this,avartar = avartar;
+}
+
+var author = new User('Dang', 'Dinh Loc', 'avartar');
+var author2 = new User('Nguyen', 'Van Loc', 'avartar');
+
+console.log(author);
+console.log(author2);
+
+
+
+
+
+
